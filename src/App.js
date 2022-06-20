11bfd043f9b403be203f4505caebc33ad9cf8e97
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2022-06-20 04:28:26 +0100

    completed coreection suggestions 70%

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     try {
       const res = await fetch(`https://api.datamuse.com/words?lc=${searchValue}`);
       const data = await res.json();
-      setNextWordSug(data[0].word);
+      if (data.length > 1) setNextWordSug(data[0].word);
     } catch (err) {
       alert('Error occured while getting susggestions!');
     }
@@ -22,19 +22,25 @@ function App() {
   const getCoreectionSugs = async () => {
     try {
       const res = await fetch(`https://api.datamuse.com/words?sp=${searchValue}`);
-      const _data = await res.json();
+      const data = await res.json();
+      let _data = [];
       for (let i = 0; i < 3; i++) {
-        // setCorrectionSugs(prevState => prevState.push(data[i]));
-        if (_data.length >= 3) console.log(_data[i]);
+        if (data.length >= 3) {
+          if (data[0].word === searchValue.toLowerCase()) data.shift();
+          _data.push(data[i].word);
+          console.log(_data);
+        }
+        setCorrectionSugs(_data);
       }
     } catch (err) {
       alert('Error occured while getting susggestions!');
+      console.log(err);
     }
   }
   
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
-    if (e.target.value.length > 3 && e.target.value.endsWith(' ')) setCanSearch(true);
+    if (e.target.value.length >= 3 && e.target.value.endsWith(' ')) setCanSearch(true);
     else setCanSearch(false);
   }
 
@@ -49,7 +55,7 @@ function App() {
     <div className="App">
       <h1 className="App__header">Typo Friendly Search Demo </h1>
       <SearchBar handleSearch={handleSearch} nextWord={nextWordSug} searchValue={searchValue}/>
-      <Suggestion correctionSugs={correctionSugs} />
+      <Suggestion correctionSugs={correctionSugs} searchValue={searchValue}/>
     </div>
   );
 }
--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Suggestion.css';
 
-function Suggestion({ correctionSugs }) {
+function Suggestion({ correctionSugs, searchValue }) {
   return (
     <>
     {
-      correctionSugs.length > 1 &&
+      searchValue && correctionSugs.length > 1 &&
       <div className="suggestion">
           <h4 className="suggestion__header">Did you mean?</h4>
           {
             correctionSugs.map(item => {
-              return <p className="suggestion__sug">{item.word}</p>
+              return <p className="suggestion__sug" key={item}>{item}</p>
             })
           }
       </div>
